9603e248bc9479a73e8baa01f89325cf29790cce
Andy Tracy <adtme11@gmail.com>
2017-10-19 16:56:45 -0400

    Tweaked things a little

--- youtube-scraper/scrape.sh
+++ youtube-scraper/scrape.sh
@@ -3,9 +3,13 @@
 # Check input
 if [ $# -eq 0 ]
 then
-	echo "Please enter a URL"
-	exit 1
+	#echo "Please enter a URL"
+	#exit 1
+
+	# Download example for now
+	youtube-dl --extract-audio --audio-format best -o "./downloads/%(title)s_%(format)s.%(ext)s" https://www.youtube.com/watch?v=LyUJIC6I7ic
+else
+	# Download audio from videos
+	youtube-dl --extract-audio --audio-format best -o "./downloads/%(title)s_%(format)s.%(ext)s" $1
 fi
 
-
-youtube-dl --extract-audio --audio-format mp3 -o "./downloads/%(title)s_%(format)s.%(ext)s" $1
