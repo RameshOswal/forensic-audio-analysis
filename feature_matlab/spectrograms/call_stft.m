221892c14aa822cf233c44848a1fd010669cc499
Bailey <yichingl@andrew.cmu.edu>
2017-12-05 04:40:31 -0500

    finalized features matlab

--- feature_matlab/spectrograms/call_stft.m
+++ feature_matlab/spectrograms/call_stft.m
@@ -1,11 +1,22 @@
-function [s_vects, spec_size  ] = call_stft( x, fs)
+function [s_vects] = call_stft(AUDIO_PATH)
 % spec size optional as output
 
+
+    disp = 0;
+    
+    samp_rate = 44100;
+    [x,fs] = audioread(AUDIO_PATH); %5 x 51143 gives 2 second long in each row
+    x2 = mean(x,2);
+    if fs ~= samp_rate
+        x2 = resample(x2,samp_rate,fs);
+        fs = samp_rate;
+    end
 
     D = 3;
     x2s = decimate(x,D);
     fs = fs/D;
     nw  = round(.02*fs);
+    M = floor((nw-1)/4); %since we are using Hamming windows
 
     w = hamming(nw);
     
@@ -14,38 +25,41 @@ function [s_vects, spec_size  ] = call_stft( x, fs)
     
     start_ind = 1;
     
-    disp = 1;
+    
     
     % take specgram of 2 sec long windows
     for cur_seg = 1:n_seg
         end_ind = start_ind + seg_len -1;
+%         if disp
+%             figure;
+%             spectrogram(x2s(start_ind:end_ind),w,round(nw/2),[],fs,'yaxis');
+%             title(['frame' num2str(cur_seg)]);
+%             colormap jet
+%         end
+        
+        % return magnitude
+        % vectorize each time segment along cols and add to matrix
+        mag_spec = 20*log10(abs(spectrogram(x2s(start_ind:end_ind),w,round(nw/2),[],fs,'yaxis')));
+
+        mag_spec = mag_spec(end:-1:1,:); %flip across xaxis
+        
         if disp
+%             figure;
+%             imagesc(1:1/fs:2,mag_spec);
+%             xlabel('time (s)');
             figure;
             spectrogram(x2s(start_ind:end_ind),w,round(nw/2),[],fs,'yaxis');
             title(['frame' num2str(cur_seg)]);
             colormap jet
         end
-        
-        % return magnitude
-        % vectorize each time segment along cols and add to matrix
-        mag_spec = 20*log10(abs(spectrogram(x2s(start_ind:end_ind),w,round(nw/2),[],fs,'yaxis')));
-        mag_spec = mag_spec(end:-1:1,:); %flip across xaxis
 
-        s_vects(cur_seg,:) =  reshape(mag_spec, 1, numel(mag_spec));
+        ms_mat(:,:,cur_seg) = mag_spec;
         start_ind = start_ind + seg_len;
     end
-    
     spec_size = size(mag_spec);
+    s_vects = generate_delta_mat(ms_mat);
     
     
-    
-    % vectorize and return magnitude
-    
-    
-    
-%     figure;
-%     imagesc(20*log10(abs(s(end:-1:1,:))));
-%     colormap jet
     
 end
 
