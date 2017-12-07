f14271515ebc5e4e7eedb262df9418abc06f3af2
Bailey <yichingl@andrew.cmu.edu>
2017-12-07 00:09:41 -0500

    updated test.py

--- feature_matlab/test.py
+++ feature_matlab/test.py
@@ -3,9 +3,10 @@ import librosa.display
 import numpy as np
 import matplotlib.pyplot as plt
 
-x,fs = librosa.load('resources/heli_and_boat_short/heli/heli0_short.wav', sr = 44100, mono = True)
+D = 44100/3
+x,fs = librosa.load('resources/heli_and_boat_short/heli/heli0_short.wav', sr = D, mono = True)
 nw = int(np.round(.02*fs))
-stft_mag = np.abs(librosa.core.stft(x, nw, int(np.round(nw/2)), window = 'hamming'))
+stft_mag = 20*np.log10(np.abs(librosa.core.stft(x, n_fft=nw, hop_length = int(np.round(nw/2)), window = 'hamming')))
 librosa.display.specshow(librosa.amplitude_to_db(stft_mag, ref = np.max), y_axis = 'log', x_axis = 'time');
 
 print(stft_mag.shape)
